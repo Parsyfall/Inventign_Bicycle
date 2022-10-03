97f75157fe6fe7c630d108f864a68ff7d1d82a4c
Parsyfall <72997097+Parsyfall@users.noreply.github.com>
2022-10-03 23:55:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,85 +1,114 @@
 #include <iostream>
+#include <vector>
 
-void string_to_int(const std::string& , int& , int& );
-/*  
-    std::stoi might throw an exeption of type "std::invalid_argument" in case argument can't be converted
-    which could be used to handle wrong input
-*/
-void set_choice(char* , int& , int& , char );
-void get_coordonates(std::istream& , std::string&);
-bool valid(char** ,int& , int&);
-bool out_of_bounds(int& , int&);
+bool find(std::vector<char> &, char);
+void set_choice(std::vector<char> &, int &, int &);
+bool valid(std::vector<char> &, int &);
+bool in_bounds(int &);
+bool Win(std::vector<char> &);
+void PrintTable(std::vector<char> &);
 
 int main() {
-    //TODO: use an STL container
-    char *matrix = new char[9] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-    int x = 0, y = 0;
-    std::string coordonates = "";
-    
-    while (true) {
-        // get the choice of the first player
-        std::cout << "Player 1: ";
-        get_coordonates(std::cin, coordonates);
-        
-        // Transform string in coordonates and verify if desired position is available
-        string_to_int(coordonates, x, y);
-        set_choice(matrix, x, y, 'X');
+  std::vector<char> table(9, ' ');
+  int cell = 0, round = 0;
 
-        std::cout << "Player 2: ";
-        get_coordonates(std::cin, coordonates);
-        string_to_int(coordonates, x, y);
-        set_choice(matrix, x, y, 'O');
+  do {
+    for (int i = 1; i <= 2; ++i) {
+      std::cout << "Player " << i << ": ";
+      do {
+        std::cin >> cell;
+      } while (!valid(table, cell));
 
-
-
-        std::cout << " " << matrix[0] << " | " << matrix[1] << " | " << matrix[2] << "\n";
-        std::cout << "---+---+--- \n";
-        std::cout << " " << matrix[3] << " | " << matrix[4] << " | " << matrix[5] << "\n";
-        std::cout << "---+---+--- \n";
-        std::cout << " " << matrix[6] << " | " << matrix[7] << " | " << matrix[8] << "\n";
+      set_choice(table, cell, i);
+      PrintTable(table);
+      if (++round >= 5) {
+        // check for winner of draw
+        if (Win(table)) {
+          return 0;
+        }
+      }
     }
-    
-    
-    
-    delete[] matrix;
+  } while (true);
 }
 
-/*  TODO:
-    # Use Matrix library instead of dynamic allocated memory
-
-    # Ask every player for a coordonate that will corespond to the certan cell in the matrimatrix[] 
-        EX. 02 will be the cell from first row and third column
-        * It can be read as a string of 2 chars then deduced as coordonates
-        * Players can't marks cell that are already used or out of bounds
-        
-    # Make an algorithm that will verify when a player win or ist's a draw 
-        Remark: This check should beging from the third round of the game (every player choice is considered a round, game consists of MIN = 3 rounds and MAX = 9)
-    # Render the table until there are no more empty cells left (Draw)
-    # Handle std::stoi exeptions for input correction
-    # Cell availability and out of matrix can be handled throgh custom exceptions
-*/
-
-void set_choice(char* matrix, int& x, int& y, char ch) {
-    matrix[(x * 3) + y] = ch;
+void set_choice(std::vector<char> &table, int &cell, int &i) {
+  char ch = (i == 1) ? 'X' : 'O';
+  table[cell] = ch;
 }
 
-void string_to_int(const std::string& str, int& x, int& y) {
-    int tmp = std::stoi(str);
-    x = tmp / 10;
-    y = tmp % 10;
+void PrintTable(std::vector<char> &table) {
+  std::cout << " " << table[0] << " | " << table[1] << " | " << table[2]
+            << "\n";
+  std::cout << "---+---+--- \n";
+  std::cout << " " << table[3] << " | " << table[4] << " | " << table[5]
+            << "\n";
+  std::cout << "---+---+--- \n";
+  std::cout << " " << table[6] << " | " << table[7] << " | " << table[8]
+            << "\n";
 }
 
-void get_coordonates(std::istream& in, std::string& str) {
-    for(size_t i = 0; i < 2; ++i) {
-        in >> str[i];
+bool Win(std::vector<char> &table) {
+  // check rows
+  for (int i = 0; i < 3; ++i) {
+    if ((table[i * 3] == table[i * 3 + 1]) &&
+        table[i * 3 + 1] == table[i * 3 + 2]) {
+      std::cout << "Player " << table[i] << " won!\n";
+      return true;
     }
-    in.sync();  //clear buffer
+  }
+
+  // check colums
+  for (int i = 0; i < 3; ++i) {
+    if (table[i] == table[i + 3] && table[i + 3] == table[i + 6]) {
+      std::cout << "Player " << table[i] << " won!\n";
+      return true;
+    }
+  }
+
+  // check main diagonal
+  if (table[0] == table[4] && table[4] == table[8]) {
+    std::cout << "Player " << table[0] << " won!\n";
+    return true;
+  }
+
+  // check secondary diagonal
+  if (table[5] == table[4] && table[4] == table[2]) {
+    std::cout << "Player " << table[2] << " won!\n";
+    return true;
+  }
+
+  // check draw
+  if (!find(table, ' ')) {
+    std::cout << "\nDraw\n";
+    std::terminate();
+  }
+
+  // default
+  return false;
 }
 
-bool out_of_bounds(int& x, int& y) {
-    return (x < 3 && y < 3) ? true : false;
+bool find(std::vector<char> &table, char key) {
+  for (std::vector<char>::iterator it = table.begin(); it < table.end(); ++it) {
+    if (*it == key)
+      return true;
+  }
+  return false;
 }
 
-bool valid(char** matrix, int& x, int& y) {
-   return (*matrix[3 * x + y] != ' ') ?  true :  false;
-}   //is cell in position (x, y) valid or not
+bool in_bounds(int &x) {
+
+  if (x >= 0 && x < 9) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool valid(std::vector<char> &table, int &cell) {
+  if (in_bounds(cell) && table[cell] == ' ') {
+    return true;
+  } else {
+    std::cout << "\nInvalid option (ocupied or inexistent cell), try again\n";
+    return false;
+  }
+}
